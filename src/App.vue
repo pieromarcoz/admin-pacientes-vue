0fd211da17651e76df77fc4dac7f0ddeb710c4a1
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-09-20 02:40:10 -0500

    component header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <div class="bg-white">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto py-20">
+    <Header />
   </div>
 </template>
 
